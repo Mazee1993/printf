4782f838d665570808fc615bae0e0ad12d6e6248
Akinrele Oluwabusayo Taye <36778872+Racta-1@users.noreply.github.com>
2023-05-29 16:40:34 +0100

    Update width.c

--- width.c
+++ width.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
  * get_width - a function that calculates the width for printing
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
- * Return: int width.
+ * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
 {
