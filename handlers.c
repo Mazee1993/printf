e5f7f3500acbed9b78092238ed0056e4b278c395
Akinrele Oluwabusayo Taye <36778872+Racta-1@users.noreply.github.com>
2023-05-29 16:13:06 +0100

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -14,7 +14,8 @@
  */
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
-{ /* char is stored at left and paddind at buffer's right */
+{ 
+	/* char is stored at left and paddind at buffer's right */
 	int i = 0;
 	char padd = ' ';
 
@@ -95,6 +96,7 @@ int write_num(int ind, char buffer[],
 	int flags, int width, int prec,
 	int length, char padd, char extra_c)
 {
+	/* number is stored at left and paddind at buffer's right */
 	int i, padd_start = 1;
 
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
