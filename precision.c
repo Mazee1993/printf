c392512c68aa01069444434e16bf93a64324c227
Akinrele Oluwabusayo Taye <36778872+Racta-1@users.noreply.github.com>
2023-05-29 16:16:28 +0100

    Update precision.c

--- precision.c
+++ precision.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
+ * get_precision - a function that calculates the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
@@ -36,6 +36,6 @@ int get_precision(const char *format, int *i, va_list list)
 	}
 
 	*i = curr_i - 1;
-
+	/* return function */
 	return (precision);
 }
